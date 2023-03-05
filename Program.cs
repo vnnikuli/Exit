aa02309dd89937db1062f9b2bbff57f5779c59f2
Vadim <yrl.nikulin.v@gmail>
2023-03-05 13:52:25 +0300

    метод

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
